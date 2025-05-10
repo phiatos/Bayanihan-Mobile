545dbe24360171eeba8d2875c53bf1b5b1f27775
thana <110603688+louihv@users.noreply.github.com>
2025-05-10 12:57:32 +0800

    comment

--- Bayanihan-Mobile/components/LaunchScreen.js
+++ Bayanihan-Mobile/components/LaunchScreen.js
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { Image, Text, TouchableOpacity, View, StyleSheet } from "react-native";
-import angatbuhaylogo from '../Graphics/Images/angatbuhay_logo.png'
+import angatbuhaylogo from '../assets/images/angatbuhay_logo.png'; 
 
 const LaunchScreen = ({ navigation }) => {
   return (
